089c19b107b3fb57abfc660873c4b3b90cd9b34b
Gavin Davenport <gavinkdavenport@gmail.com>
2023-09-22 11:30:07 -0700

    Add pseudocode
    Add pseudocode for playing a round

--- script.js
+++ script.js
@@ -11,5 +11,15 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     // make playerSelection all lower case
-    
+    // if playerSelection = "rock" and computerSelection = "rock", return "It's a draw!"
+    // if playerSelection = "rock" and computerSelection = "paper", return "You lose! Paper beats Rock"
+    // if playerSelection = "rock" and computerSelection = "scissors", return "You win! Rock beats Scissors"
+    // if playerSelection = "paper" and computerSelection = "rock", return "You win! Paper beats Rock"
+    // if playerSelection = "paper" and computerSelection = "paper", return "It's a draw!"
+    // if playerSelection = "paper" and computerSelection = "scissors", return "You lose! Scissors beats Paper"
+    // if playerSelection = "scissors" and computerSelection = "rock", return "You lose! Rock beats Scissors"
+    // if playerSelection = "scissors" and computerSelection = "paper", return "You win! Scissors beats Paper"
+    // if playerSelection = "scissors" and computerSelection = "scissors", return "It's a draw!"
 }
+
+
