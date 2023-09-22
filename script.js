62d0480ae0de3acf8352862e0adfdc030c5dcde2
Gavin Davenport <gavinkdavenport@gmail.com>
2023-09-22 11:17:31 -0700

    Add functions and pseudocode
    Comment the basic logic of the functions needed for the game

---
+++ script.js
@@ -0,0 +1,15 @@
+function getComputerChoice() {
+    // Create a variable computerSelection
+    // Choose a random integer 1, 2, or 3
+    // if 1, make computerSelection "rock"
+    // if 2, make computerSelection "paper"
+    // if 3, make computerSelection "scissors"
+}
+
+// create a constant playerSelection
+// Player inputs "rock", "paper", or "scissors" into playerSelection
+
+function playRound(playerSelection, computerSelection) {
+    // make playerSelection all lower case
+    
+}
